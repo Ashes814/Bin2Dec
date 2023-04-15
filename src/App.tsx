6d6cc3cc1e0b8c9952def0bc947ad7a6d8a27f9e
张欧文 <1534661514@qq.com>
2023-04-15 10:34:03 +0800

    implement core function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./App.css";
+import "./App.scss";
 import B2DCalculator from "./components/B2DCalculator";
 
 function App() {
--- src/components/B2DCalculator/index.tsx
+++ src/components/B2DCalculator/index.tsx
@@ -2,19 +2,42 @@ import React, { useState, useRef } from "react";
 import styles from "./B2DCalculator.module.scss";
 
 export default function B2DCalculator() {
-  const binaryInputRef = useRef(null);
+  const [decimal, setDecimal] = useState(0);
+  const binaryInputRef = useRef<HTMLInputElement>(null);
+
   const binary2Decimal = (e: React.FormEvent): void => {
     e.preventDefault();
+    const inputValue = binaryInputRef.current?.value;
 
-    console.log(binaryInputRef.current?.value);
+    if (!inputValue) {
+      alert("请输入内容");
+      return;
+    }
+
+    let decimalValue = 0;
+    for (let i = 0; i < inputValue.length; i++) {
+      decimalValue += +inputValue[i] * 2 ** (inputValue.length - i - 1);
+    }
+    setDecimal(decimalValue);
+    binaryInputRef.current.value = "";
+  };
+
+  const validateInput = (event: React.KeyboardEvent): void => {
+    if (event.key == "1" || event.key == "0") {
+      return;
+    } else {
+      event.preventDefault();
+      alert("二进制输入只能是0或1");
+    }
   };
 
   return (
     <div className={styles.Container}>
       <form>
-        <input type="text" ref={binaryInputRef} />
+        <input type="text" ref={binaryInputRef} onKeyDown={validateInput} />
         <button onClick={binary2Decimal}>开始转换</button>
       </form>
+      <div>{decimal}</div>
     </div>
   );
 }
